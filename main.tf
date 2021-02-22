2744e7f51096652015839eb1215cd4c3a98eda3c
Srini Maddineni <maddinenisri@gmail.com>
2021-02-21 23:11:51 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   resource_group_name             = azurerm_resource_group.demo.name
   dns_prefix                      = "devaks"
   kubernetes_version              = "1.18.14"
-  node_resource_group             = "dev-aks-worker"
+  # node_resource_group             = "dev-aks-worker"
   # private_cluster_enabled         = var.private_cluster
   # sku_tier                        = var.sla_sku
   # api_server_authorized_ip_ranges = var.api_auth_ips
@@ -118,6 +118,9 @@ resource "azurerm_log_analytics_workspace" "analytics_workspace" {
   sku                 = "PerGB2018"
 }
 
+data "azurerm_resource_group" "aks_node_rg" {
+  name = azurerm_kubernetes_cluster.aks.node_resource_group
+}
 # resource "azurerm_kubernetes_cluster_node_pool" "aks" {
 #   lifecycle {
 #     ignore_changes = [
@@ -163,9 +166,64 @@ resource "azurerm_role_assignment" "aks_subnet" {
 #   principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
 # }
 
+# resource "kubernetes_namespace" "csi" {
+#   metadata {
+#     annotations = {
+#       name = "csi"
+#     }
+#     name = "csi"
+#   }
+# }
+
+resource "azurerm_role_assignment" "vm_contributor" {
+  scope                = data.azurerm_resource_group.aks_node_rg.id
+  role_definition_name = "Virtual Machine Contributor"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+  depends_on = [
+    azurerm_kubernetes_cluster.aks
+  ]
+}
+
+resource "azurerm_role_assignment" "all_mi_operator" {
+  scope                = data.azurerm_resource_group.aks_node_rg.id
+  role_definition_name = "Managed Identity Operator"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+  depends_on = [
+    azurerm_kubernetes_cluster.aks
+  ]
+
+}
+
+# resource "azurerm_role_assignment" "rg_vm_contributor" {
+#   scope                = azurerm_resource_group.demo.id
+#   role_definition_name = "Virtual Machine Contributor"
+#   principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+#   depends_on = [
+#     azurerm_kubernetes_cluster.aks
+#   ]
+# }
+
+resource "azurerm_role_assignment" "rg_all_mi_operator" {
+  scope                = azurerm_resource_group.demo.id
+  role_definition_name = "Managed Identity Operator"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+  depends_on = [
+    azurerm_kubernetes_cluster.aks
+  ]
+}
+
+resource "azurerm_role_assignment" "mi_operator" {
+  scope                = azurerm_user_assigned_identity.mi.id
+  role_definition_name = "Managed Identity Operator"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+  depends_on = [
+    azurerm_kubernetes_cluster.aks
+  ]
+
+}
+
 provider "kubernetes" {
-  load_config_file       = false
-  host                   = format("https://%s:443", azurerm_kubernetes_cluster.aks.private_fqdn)
+  host                   = "${azurerm_kubernetes_cluster.aks.kube_admin_config.0.host}"
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].cluster_ca_certificate)
   client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_certificate)
   client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_key)
@@ -182,11 +240,11 @@ provider "helm" {
   }
 }
 
-resource "helm_release" "example" {
-  # kube_config = azurerm_kubernetes_cluster.aks.kube_config_raw
-  name  = "redis"
-  chart = "https://charts.bitnami.com/bitnami/redis-10.7.16.tgz"
-}
+# resource "helm_release" "example" {
+#   # kube_config = azurerm_kubernetes_cluster.aks.kube_config_raw
+#   name  = "redis"
+#   chart = "https://charts.bitnami.com/bitnami/redis-10.7.16.tgz"
+# }
 
 
 # resource "helm_release" "ingress" {
@@ -198,3 +256,141 @@ resource "helm_release" "example" {
 #         value = "true"
 #     }
 # }
+
+resource "helm_release" "csi" {
+  name = "csi"
+  namespace = "csi"
+  repository = "https://raw.githubusercontent.com/Azure/secrets-store-csi-driver-provider-azure/master/charts"
+  chart = "csi-secrets-store-provider-azure"
+  version = "0.0.17"
+  create_namespace = true
+}
+
+resource "helm_release" "aad_pod_id" {
+  name             = "aad-pod-identity"
+  repository       = "https://raw.githubusercontent.com/Azure/aad-pod-identity/master/charts"
+  chart            = "aad-pod-identity"
+  version          = "3.0.3"
+  namespace        = "aad-pod-id"
+  create_namespace = true
+
+  values = [yamlencode(local.values_config)]
+
+  depends_on = [azurerm_kubernetes_cluster.aks, azurerm_user_assigned_identity.mi]
+}
+
+locals {
+  values_config = {
+    azureidentity = [
+      {
+        name = "azure-identity"
+        type = 0
+        resourceID = "${azurerm_user_assigned_identity.mi.id}"
+        clientID = "${azurerm_user_assigned_identity.mi.client_id}"
+        binding = {
+          name = "azure-identity-binding"
+          selector = "aad-pod-id-binding-selector"
+        }
+      }
+    ]
+  }
+}
+
+# resource "kubernetes_manifest" "ai-crd" {
+#   provider = "kubernetes"
+#   manifest = {
+#     apiVersion = "aadpodidentity.k8s.io/v1"
+#     kind = AzureIdentity
+#     metadata = {
+#       name: "azure-identity"
+#     }
+#     spec = {
+#       clientID = "${azurerm_user_assigned_identity.mi.client_id}"
+#       resourceID = "${azurerm_user_assigned_identity.mi.id}"
+#       type = 0
+#     }
+#   }
+# }
+
+# resource "kubernetes_manifest" "ai-binding-crd" {
+#   provider = "kubernetes"
+#   manifest = {
+#     apiVersion = "aadpodidentity.k8s.io/v1"
+#     kind = AzureIdentityBinding
+#     metadata = {
+#       name: "azure-identity-binding"
+#     }
+#     spec = {
+#       azureIdentity = "azure-identity"
+#       selector = "aad-pod-id-binding-selector"
+#     }
+#   }
+# }
+
+resource "azurerm_user_assigned_identity" "mi" {
+  name                = "mi-${random_string.unique.result}"
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
+}
+
+resource "random_string" "unique" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "kv" {
+  name                = "kv-${random_string.unique.result}"
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+
+  network_acls {
+    default_action = "Allow"
+    bypass         = "AzureServices"
+  }
+}
+
+resource "azurerm_key_vault_access_policy" "kv" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set",
+    "Delete"
+  ]
+  depends_on = [azurerm_key_vault.kv]
+}
+
+resource "azurerm_role_assignment" "kv" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Reader"
+  principal_id         = azurerm_user_assigned_identity.mi.principal_id
+}
+
+# resource "azurerm_role_assignment" "node_rg_reader" {
+#   scope                = data.azurerm_resource_group.aks_node_rg.id
+#   role_definition_name = "Reader"
+#   principal_id         = azurerm_user_assigned_identity.mi.principal_id
+# }
+
+resource "azurerm_key_vault_access_policy" "mi" {
+  key_vault_id       = azurerm_key_vault.kv.id
+  tenant_id          = data.azurerm_client_config.current.tenant_id
+  object_id          = azurerm_user_assigned_identity.mi.principal_id
+  secret_permissions = ["Get"]
+}
+
+resource "azurerm_key_vault_secret" "demo" {
+  name         = "demo-secret"
+  value        = "demo-value"
+  key_vault_id = azurerm_key_vault.kv.id
+
+  # Must wait for Terraform SP policy to kick in before creating secrets
+  depends_on = [azurerm_key_vault_access_policy.kv]
+}
